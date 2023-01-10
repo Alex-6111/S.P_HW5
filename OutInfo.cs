5b272426bcad8e098d5d7257709a8f7822787659
Oleksandr Bekas <lxbekas@gmail.com>
2023-01-10 17:36:09 +0200

    Add project files.

---
+++ OutInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S.P_HW5
+{
+    public class OutInfo
+    {
+        public bool IsSentenseAmount { get; set; }
+        public bool IsCharacterAmount { get; set; }
+        public bool IsWordsAmount { get; set; }
+        public bool IsInterrogativeSentensesAmount { get; set; }
+        public bool IsImperativeSentenseAmount { get; set; }
+    }
+}
